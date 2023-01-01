012017c9f75b3feb409ca4bbb0544496d829048b
prajwal <prajwalkandekar123456@gmail.com>
2023-01-01 11:05:36 +0530

    13th commit

---
+++ 15_partition_equal_subset_sum.cpp
@@ -0,0 +1,125 @@
+// Problem
+/*
+We are given an array ‘ARR’ with N positive integers. We need to find if we can partition the array into two subsets such that the sum of elements of each subset is equal to the other.
+
+If we can partition, return true else return false
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution{
+private:
+    // Recursion
+    // Time COmplexity : O(2^N)
+    // Space Complexity : O(N)
+    bool recc(int index,int target,vector<int> &arr)
+    {
+        if(target == 0) return true;
+        if(index == 0) return arr[index] == target;
+        bool notTake = recc(index-1,target,arr);
+        bool take = false;
+        if(target>=arr[index])
+        {
+            take = recc(index-1,target-arr[index],arr);
+        }
+        return take||notTake;
+    }
+
+    // Memoization
+    // Time Complexity : O(N*K)
+    // Space Complexity : O(N*K)+O(N)
+    bool memo(int index,int target,vector<int> &arr,vector<vector<int>> &dp)
+    {
+        if(target == 0) return true;
+        if(index == 0) return arr[index]==target;
+        if(dp[index][target]!=-1) return dp[index][target];
+        bool notTake = memo(index-1,target,arr,dp);
+        bool take = false;
+        if(target>=arr[index])
+        {
+            take = memo(index-1,target-arr[index],arr,dp);
+        }
+        return dp[index][target] = take || notTake;
+    }
+
+    // Tabulation
+    // Time Complexity : O(N*K)
+    // Space Complexity : O(N*K)
+    bool tab(int n,int k,vector<int> &arr)
+    {
+        vector<vector<bool>> dp(n,vector<bool>(k+1,false));
+        for(int i=0; i<n; i++){
+            dp[i][0] = true;
+         }
+    
+        if(arr[0]<=k)
+            dp[0][arr[0]] = true;
+        for(int index = 1;index<n;index++)
+        {
+            for(int target = 1;target<=k;target++)
+            {
+                bool notTake = dp[index-1][target];
+                bool take = false;
+                if(target>=arr[index])
+                {
+                    take = dp[index-1][target-arr[index]];
+                }
+                dp[index][target] = take||notTake;
+            }
+        }
+        return dp[n-1][k];
+    }
+
+    // Space Optimization
+    // Time Complexity :O(N*K)
+    // Space COmplexity : O(N)
+    int opti(int n,int k,vector<int> &arr)
+    {
+        vector<bool> prev(k+1,0);
+        prev[0] = true;
+        if(arr[0]<=k)
+            prev[arr[0]] = true;
+        for(int index = 1;index<n;index++)
+        {
+            vector<bool> curr(k+1,false);
+            curr[0] = true;
+            for(int target = 1;target<=k;target++)
+            {
+                bool notTake = prev[target];
+                bool take = false;
+                if(target>=arr[index])
+                {
+                    take = prev[target-arr[index]];
+                }
+                curr[target] = take || notTake;
+            }
+            prev = curr;
+        }
+        return prev[k];
+    }
+public:
+    bool subsetSumToK(int n,int k,vector<int> &arr)
+    {
+        int totalSum = 0;
+    for(int i = 0;i<n;i++) totalSum+=arr[i];
+    int target = totalSum/2;
+    if(totalSum%2 != 0) return false;
+    vector<vector<int>>dp(n,vector<int>(target+1,-1));
+    return memo(n-1,target,arr,dp);
+    }
+};
+
+int main()
+{
+    int n,k;
+    cin>>n>>k;
+    vector<int> arr(n);
+    for(int i = 0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    Solution obj;
+    cout<<obj.subsetSumToK(n,k,arr)<<endl;
+    return 0;
+}
